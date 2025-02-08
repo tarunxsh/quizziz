50f2abd436067400245ed290fdeb396177e75a34
tarunxsh <taruneshkumar1234@gmail.com>
2025-02-08 16:28:43 +0530

    chore: setup mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
-import 'reflect-metadata';
 import express from "express";
 import userRoutes from "./routes/users";
 import gameRoutes from "./routes/games";
-import { initDb } from "./config/db";
-
-
+import { connectDB } from "./config/db";
 
 const app = express();
 app.use(express.json());
-initDb();
+
 
 const router = express.Router();
 router.use("/users", userRoutes);
 router.use("/game", gameRoutes);
 app.use("/api/v1", router);
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
