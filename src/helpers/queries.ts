5375b89ff33676815418e919a530c48dcbfca02d
tarunxsh <taruneshkumar1234@gmail.com>
2025-02-09 15:30:26 +0530

    chore: add common queries

--- src/helpers/queries.ts
+++ src/helpers/queries.ts
@@ -1,4 +1,6 @@
 import user from '../models/user.model';
+import Questions from '../models/question.model';
+import Game from '../models/game.model';
 
 export const getUser = async ({username}: any) => {
     return await user.findOne({ username });
@@ -7,3 +9,18 @@ export const getUser = async ({username}: any) => {
 export const createUser = async ({username, email, password}: any) => {
     return await user.create({ username, email, password });
 }
+
+export const getGame = async ({game_id}: any) => {
+    return Game.findOne({
+        _id: game_id
+    });
+}
+
+export const getQuestion = async ({questionNumber, question_id}: any) => {
+  const question = await Questions.findOne({
+      ...(questionNumber && { questionNumber }),
+      ...(question_id && { _id: question_id }),
+    }
+  );
+  return question || null;
+};
